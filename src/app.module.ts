ec5c2083f97871c579cd169be84b75bd197dd39c
RizqiSyahrendra <rizqialaudinsyahrendra@gmail.com>
2025-11-13 16:11:04 +0700

    feat: logging system

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,51 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+import DiscordTransport from 'winston-discord-transport';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    WinstonModule.forRoot({
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.colorize(),
+            winston.format.printf(({ timestamp, level, message, context }) => {
+              return `${timestamp} [${context}] ${level}: ${message}`;
+            }),
+          ),
+        }),
+        new winston.transports.File({
+          filename: 'logs/all.log',
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.json(),
+          ),
+        }),
+        new winston.transports.File({
+          filename: 'logs/error.log',
+          level: 'error',
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.json(),
+          ),
+        }),
+        ...(!!process.env.DISCORD_LOG_WEBHOOK
+          ? [
+              new DiscordTransport({
+                webhook: process.env.DISCORD_LOG_WEBHOOK || `-`,
+                defaultMeta: { service: 'hi-fella-shoe-api' },
+                level: 'error',
+              }),
+            ]
+          : []),
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
